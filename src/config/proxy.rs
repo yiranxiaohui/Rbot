0d017936320b37fed563bde87dbf0246eeb92106
周宏辉 <zhouhonghui@dolphin-ai.cn>
2025-09-30 12:14:49 +0800

    add：新增代理开关

--- src/config/proxy.rs
+++ src/config/proxy.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Clone, Serialize)]
 pub struct Proxy {
+    pub enabled: bool,
     pub address: String,
     pub port: u16,
 }
--- src/utils/request.rs
+++ src/utils/request.rs
@@ -7,11 +7,16 @@ static CLIENT_CELL: OnceCell<Client> = OnceCell::const_new();
 pub async fn get_client() -> &'static Client {
     CLIENT_CELL.get_or_init(|| async {
         let config = get_config_clone().await;
-        let scheme = format!("socks5://{}:{}", config.proxy.address, config.proxy.port);
-        let proxy = reqwest::Proxy::all(scheme).unwrap();
-        Client::builder()
-            .proxy(proxy)
-            .build()
-            .unwrap()
+        if (config.proxy.enabled) {
+            let scheme = format!("socks5://{}:{}", config.proxy.address, config.proxy.port);
+            let proxy = reqwest::Proxy::all(scheme).unwrap();
+            Client::builder()
+                .proxy(proxy)
+                .build()
+                .unwrap()
+        } else {
+            Client::new()
+        }
+
     }).await
 }

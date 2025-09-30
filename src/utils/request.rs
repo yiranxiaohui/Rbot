7b0dd1e37dd1eb424c9b9d5ff5c75f44e8baeb74
周宏辉 <zhouhonghui@dolphin-ai.cn>
2025-09-30 12:17:44 +0800

    remove：删除if括号

--- src/utils/request.rs
+++ src/utils/request.rs
@@ -7,7 +7,7 @@ static CLIENT_CELL: OnceCell<Client> = OnceCell::const_new();
 pub async fn get_client() -> &'static Client {
     CLIENT_CELL.get_or_init(|| async {
         let config = get_config_clone().await;
-        if (config.proxy.enabled) {
+        if config.proxy.enabled {
             let scheme = format!("socks5://{}:{}", config.proxy.address, config.proxy.port);
             let proxy = reqwest::Proxy::all(scheme).unwrap();
             Client::builder()
